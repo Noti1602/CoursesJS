509daf19716452d97a3930e13fc43a7b60a8b69a
Home_3110 <bnoti1602@gmail.com>
2019-05-28 17:03:18 +0300

    First commit

---
+++ dz1/src/components/Payment.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+
+class Payment extends Component{
+    constructor(props){
+        super(props);
+    }
+    onCalculateSum()
+    {
+        const {sum, months, monthpaymentsum, monthpaymentpercent }=this.refs;
+
+        let sum1 = parseFloat(sum.value);
+        let months1 = parseInt(months.value);
+        monthpaymentsum.value=sum1/months1*(0,25/12);
+        monthpaymentpercent.value=(sum1*25/100)*months1;
+    }
+    render(){
+        return (
+                <div>
+                    <script> alert ("ok")</script>
+                    Загальна сума кредиту:
+                    <input type="text" ref="sum" />
+                    Кількість місяців кредиту:
+                    <input type="text" ref="months" />
+                    <button onClick={this.onCalculateSum.bind(this)}> Розрахувати: </button>
+
+
+
+                    <br/>
+                    Сума щомісячного платежу:
+                    <input type="text" ref="monthpaymentsum"/>
+
+
+
+                    Сума відсотків:
+                    <input type="text" ref="monthpaymentpercent"/>
+</div>
+
+
+        )
+    }
+}
+export default Payment;
